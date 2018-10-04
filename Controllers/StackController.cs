a1c05b814c74d7acaf456cf1bf86315b886fabf4
bswat01 <beswat14@gmail.com>
2018-10-03 22:45:26 -0600

    Added Files From Anderson Github
    https://github.com/byu-is-403/syllabus/blob/master/data-structures/assignment/data-structures.md

---
+++ Controllers/StackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dataStructuresGIT.Controllers
+{
+    public class StackController : Controller
+    {
+        // GET: Stack
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
